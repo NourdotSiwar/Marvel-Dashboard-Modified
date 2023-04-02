88be6339d179a59544d612ee6e77d90ffd6b6fa4
Nour Siwar <nour.siwar@outlook.com>
2023-04-02 11:48:03 -0700

    finished adding 4 charts

--- src/Components/Charts.jsx
+++ src/Components/Charts.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component, useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
 import {
       LineChart,
@@ -7,8 +7,17 @@ import {
       XAxis,
       YAxis,
       Tooltip,
+      BarChart,
+      Bar,
+      Legend,
+      ScatterChart,
+      Scatter,
+      ZAxis,
+      PieChart,
+      Pie,
+      Cell,
 } from 'recharts';
-import { Scatter } from 'react-chartjs-2';
+
 
 // this is the api key and hash from the marvel developer portal
 const API_KEY = import.meta.env.VITE_API_KEY;
@@ -16,15 +25,28 @@ const HASH = import.meta.env.VITE_HASH;
 
 const charts = () => {
       const [data, setData] = useState(null);
+      // make 20 colors with palettes of gray, white, red, black
+      const COLORS = ['#808080', '#FFFFFF', '#FF0000', '#000000', '#C0C0C0', '#808080', '#FFFFFF', '#FF0000', '#000000', '#C0C0C0', '#808080', '#FFFFFF', '#FF0000', '#000000', '#C0C0C0', '#808080', '#FFFFFF', '#FF0000', '#000000', '#C0C0C0']
+      
 
       useEffect(() => {
             const fetchData = async () => {
                   const response = await axios.get(`https://gateway.marvel.com:443/v1/public/characters?&apikey=${API_KEY}&ts=1&hash=${HASH}`)
                   setData(response.data)
-
             }
             fetchData().catch(console.error)
       }, [])
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+      cx, cy, midAngle, innerRadius, outerRadius, percent, index,
+}) => {
+      const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+      const x  = cx + radius * Math.cos(-midAngle * RADIAN);
+      const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+
+}
+
 
       return (
             <div>
@@ -44,7 +66,7 @@ const charts = () => {
                                     tick={{ fill: 'white' }} />
                                    <Tooltip formatter=
                                     {(value, name, props) => [value, 'Comics']}
-                                    labelFormatter={(label) => ['Character Name: ' + label]}
+                                    labelFormatter={(label) => ['Name: ' + label]}
                                     labelStyle={{ color: 'white' }}
                                     itemStyle={{ color: 'white' }}
                                     contentStyle={{ backgroundColor: 'black' }}
@@ -61,9 +83,100 @@ const charts = () => {
                   </h3>
                   {data ? (
                         <div className='chartDiv'>
+                              <BarChart
+                              width={1000}
+                              height={500}
+                              data={data.data.results}
+                              margin={{
+                              top: 5,
+                              right: 30,
+                              left: 20,
+                              bottom: 5
+                              }}
+                              >
+                              <CartesianGrid strokeDasharray="5 5" stroke="#ccc" />
+                              <XAxis dataKey="name" axisLine={{ stroke: 'white' }} tickLine={{ stroke: 'white'}} label={{ value: 'Character Name', position: 'insideBottom', offset: -5, fill: 'white' , fontWeight: 'bold'}} tick={{ fill: 'white' }}
+                              />
+                              <YAxis axisLine={{ stroke: 'white' }} tickLine={{ stroke: 'white' }} label={{ value: 'Number of Series', angle: -90, position: 'insideLeft', fill: 'white', fontWeight: 'bold'}}
+                              tick={{ fill: 'white' }} />
+                              <Tooltip 
+                              formatter={(value, name, props) => [value, 'Series']}
+                              labelFormatter={(label) => ['Name: ' + label]}
+                              labelStyle={{ color: 'black' }}
+                              itemStyle={{ color: 'black' }}
+                              cursor={{ stroke: 'black', strokeWidth: 1 }}
+                              />
+                              <Bar dataKey="series.available" fill="black" />
+                              </BarChart>
                         </div>
                   ) : null}
 
+                  <h1>Chart 3: Events</h1>
+                  <h3 className='chartDivSummary'>
+                        This chart shows the number of events that each character has appeared in.
+                  </h3>
+                  {data ? (
+                        <div className='chartDiv'>
+                              <ScatterChart
+                              width={1000}
+                              height={500}
+                              margin={{
+                              top: 20,
+                              right: 20,
+                              bottom: 20,
+                              left: 20
+                              }}
+                              >
+                              <CartesianGrid strokeDasharray="5 5" />
+                              <XAxis dataKey="name" type="category" name="Name" axisLine={{ stroke: 'white' }} tickLine={{ stroke: 'white'}} label={{ value: 'Character Name', position: 'insideBottom', offset: -5, fill: 'white' , fontWeight: 'bold'}} tick={{ fill: 'white' }} />
+                              <YAxis type="number" dataKey="events.available" name="Events" axisLine={{ stroke: 'white' }} tickLine={{ stroke: 'white' }} label={{ value: 'Number of Events', angle: -90, position: 'insideLeft', fill: 'white', fontWeight: 'bold'}}
+                              tick={{ fill: 'white' }} />
+                              <Tooltip
+                              formatter={(value, name, props) => [`${value}`,`${name}`, ]}
+                              labelStyle={{ color: 'white' }}
+                              itemStyle={{ color: 'white' }}
+                              contentStyle={{ backgroundColor: 'black' }}
+                              cursor={{ stroke: 'white', strokeWidth: 1 }}
+                              />
+                              <Scatter data={data.data.results} fill="white"   
+                              line
+                              shape="star"
+                              />
+                              </ScatterChart>
+                        </div>
+                  ) : null}
+
+
+                  <h1>Chart 4: Stories</h1>
+                  <h3 className='chartDivSummary'>
+                        This chart shows the number of stories that each character has appeared in.
+                  </h3>
+                  {data ? (
+                        <div className='chartDiv'>
+                              <PieChart width={1000} height={500}>
+                              <Pie
+                                    data={data.data.results}
+                                    cx={500}
+                                    cy={230}
+                                    labelLine={false}
+                                    label={renderCustomizedLabel}
+                                    outerRadius={200}
+                                    fill="#8884d8"
+                                    dataKey="stories.available"       
+                              >
+                                    {data.data.results.map((entry, index) => (
+                                          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                    ))}
+                              </Pie>
+                              <Tooltip
+                                  formatter={(value, name, props) => [`Stories: ${value}`, `Name: ${name}`]}
+                              labelStyle={{ color: 'black' }}
+                              itemStyle={{ color: 'black' }}
+                              cursor={{ stroke: 'black', strokeWidth: 1 }}
+                              />
+                              </PieChart>
+                        </div>
+                  ) : null}
             </div>
       )
 
